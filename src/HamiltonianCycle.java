f7500a85da0786c0453b2d50820226577899eacc
SedOwO <shashank21naik@gmail.com>
2023-09-13 23:36:56 +0530

    comments go brr

--- src/HamiltonianCycle.java
+++ src/HamiltonianCycle.java
@@ -25,15 +25,19 @@ public class HamiltonianCycle {
 		}
 	}
 	
-	// function to find the next value to be added
+	// function to find the next value to be added in x[k]
 	void nextValue(int k) {
 		int i = 0;
 		while(true) {
-			x[k] = x[k] + 1;
-			if(x[k] == n)
-				x[k] = 0;
-			if(x[k] == 0)
-				return;
+			x[k] = x[k] + 1;	// assign x[k] to next higest possible vertex
+			
+			if(x[k] == n) {		// x[k] has radched limit, cannot assign anymore vertex
+				x[k] = 0;		// this is important else the inifinite while loop wont stop
+				return;			// return to hamiltonian wit x[k] = 0
+			}
+			// USELESS!
+			// if(x[k] == 0)
+			// 	return;
 				
 			// if edge exists between 2 vertices in any stage
 			if(graph[x[k - 1]][x[k]])
@@ -42,23 +46,26 @@ public class HamiltonianCycle {
 						break;
 			
 			if(i == k)
-				if(k < n || k == n && graph[x[n]][1])
-					return;
+				if(k < n || (k == n && graph[x[n]][1]))	// checks for the validity of the cycle
+					return;		// return without modifying x[k]
 		}
 	}
 	
 	void hamiltonian(int k) {
 		while(true) {
 			nextValue(k);	// get the next element of next stage
-			if(x[k] == 0)
+			
+			if(x[k] == 0)	// cant assign vertex, then backtrack cuz dead-end
 				return;
+			
+			// if the recursion has gone throught all the vertices, n - 1 because: 0, 1, 2... n - 1
 			if(k == n - 1) {
 				System.out.println("\n Path");
 				for (int i = 0; i < n; i++)
 					System.out.print((x[i] + 1) + "-->");
 				System.out.println("1");
 			}
-			else 
+			else 			// not yet reached the end, then go the the k + 1 iteration
 				hamiltonian(k + 1);
 		}
 	}
@@ -70,7 +77,7 @@ public class HamiltonianCycle {
 		HamiltonianCycle obj = new HamiltonianCycle(n);
 		obj.getGraph();
 		
-		// starting from first stage
+		// starting from index 1 because the first index is alwasys 0
 		obj.hamiltonian(1);
 		
 		sc.close();
